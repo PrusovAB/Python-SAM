5394aa7069463f61a492387b8e0bd41c8409911c
Froge <maksimdis31@gmail.com>
2022-10-08 16:53:21 +0300

    1

--- GB /HomeWork/HomeWork#4/Task#4.4.py
+++ GB /HomeWork/HomeWork#4/Task#4.4.py
@@ -15,6 +15,7 @@ k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
     """
 )
 
+
 from random import choice
 
 
@@ -22,16 +23,17 @@ def polynomial(num: int):
     if num < 1:
         return 0
 
-    poly = "" # пустая строка
-    num_list = range(0, 10) # список итерированный
+    poly = ""  # пустая строка
+    num_list = range(0, 10)  # список итерированный
 
-    with open("Task44.txt", "a", encoding="utf-8") as my_f:
+    with open("Task4_1.txt", "a", encoding="utf-8") as my_f:
         for i in range(num, 0, -1):
-            value = choice(num_list) # выбрать объект из итерированного списка
-            if value: # если объект не 0
-                poly += f"{value}*x^{i} {choice('+-')} " # сделать запись в строку с выбором + -
+            value = choice(num_list)  # выбрать объект из итерированного списка
+            if value:  # если объект не 0
+                poly += f"{value}*x^{i} {choice('+-')} "  # сделать запись в строку с выбором + -
 
-        my_f.write(f"{poly}{choice(num_list)} = 0\n") # сделать запись в файл
+        my_f.write(f"{poly}{choice(num_list)} = 0\n")  # сделать запись в файл
+
 
 for _ in range(3):
     polynomial(int(input('Введите данные: ')))
